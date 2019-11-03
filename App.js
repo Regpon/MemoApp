662780e189973f1c58d3b173c19d76d36f23d40e
Regpon <yuusuke@okamoto.us>
2019-11-03 09:40:47 +0900

    Create Login & Signup Screen

--- App.js
+++ App.js
@@ -5,12 +5,14 @@ import AppBar from './src/components/AppBar';
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <MemoEditScreen />
+      <SignUpScreen />
     </View>
   );
 }
